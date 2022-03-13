41e0cfb441eba5875c0b98fe196d2ff017e1f35f
Thobias P Silva <thobiaspsilva@yahoo.com.br>
2022-03-13 02:15:48 -0300

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
-import express from 'express';
+import express, { Request, Response, Router } from 'express';
 import cors from 'cors';
+import { Telegraf } from 'telegraf';
+import 'dotenv/config';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.listen(8081, () => console.log("Server is running..."));
+app.get('/', async (req: Request, res: Response) => {
+  return res.status(200).send('Sucesso');
+});
+
+const token = process.env.BOT_TOKEN;
+const chatID = process.env.CHAT_ID;
+const bot = new Telegraf(token!);
+
+app.post('/send', async (req: Request, res: Response) => {
+  const { message }: { message: string } = req.body;
+  try {
+    const result = await bot.telegram.sendMessage(chatID!, message as string);
+    return res
+      .status(200)
+      .send({ message: 'Mensagem enviada', data: result.text });
+  } catch (error: any) {
+    return res
+      .status(error.response.error_code)
+      .send({ message: error.response.description });
+  }
+});
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log('Server is running... ', port));
